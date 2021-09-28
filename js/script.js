07c02c079466446db0e06ae648dd2886725bfb59
gordievvlad <gordievich12344@gmail.com>
2021-09-28 20:35:37 +0300

    cnanged blog

--- js/script.js
+++
@@ -1,9 +0,0 @@
-menu.onclick = function myFuntion() {
-	var x = document.getElementById("myTopnav");
-	
-	if(x.className === "topnav"){
-		x.className += " responsive";
-	}else{
-		x.className = "topnav";
-	}
-}
